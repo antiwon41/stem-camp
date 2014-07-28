1a3005dcea7491d42d1b4e7d4b7c7d8e3727cc2d
Salvatore Torcivia <salvatore.torcivia93@gmail.com>
2014-07-28 12:11:17 -0400

    Fixed links between skip and link and attach pages

--- javascript/index.js
+++ javascript/index.js
@@ -1,14 +1,3 @@
 $(document).ready(function(){
-	$("#skip").click(function(){
-		window.location.href = "skip.html";
-	});
-	$("#btn").click(function(){
-		window.location.href = "attach.html";
-	});
-	$("#btn2").click(function(){
-		window.location.href = "link.html";
-	});
-	$("#search-2").click(function(){
-		window.location.href = "";
-	});
+	
 });
